de2d06ee656e41481c6aa4b907265af543ac30ac
Navid Mesbah <mesbah.navid@gmail.com>
2020-05-08 23:35:57 +0430

    update

---
+++ component---src-pages-page-2-js-15978f2c41ae6365231f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{222:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),o=t(152),c=t(220),r=t(219);n.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,null,l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-15978f2c41ae6365231f.js.map
